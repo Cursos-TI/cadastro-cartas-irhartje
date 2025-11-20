b9a9258ba53f0bc0f457d19cf673f2eadafbc80d
irHartje <irhartje@gmail.com>
2025-11-20 15:11:53 +0000

    Organizei as variáveis de forma que separassem a carta 1 da carta 2, scaneei e imprimi os dados das mesmas, formatando de forma apropriada. Coleta os dados e exibe numa saída bem formatada. Dá ao usuário a habilidade de poder escolher e alterar os valores como achar necessário.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,11 +5,86 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+//Dados para trunfo do País 1
+  char pais1[30], codigo1[5], capital1[50];
+    int populacao1, pontos_turisticos1;
+    float area1, pib1;
 
-  // Área para entrada de dados
+//Área para dados do País 1
 
-  // Área para exibição dos dados da cidade
+    printf("Digite o País da primeira carta: \n");
+    scanf("%s", pais1);
+
+    printf("Digite o codigo da primeira carta: \n");
+    scanf("%s", codigo1);
+
+    printf("Digite a Capital da primeira carta: \n");
+    scanf("%s", capital1);
+
+    printf("Digite a populacao da primeira carta: \n");
+    scanf("%d", &populacao1);
+    
+    printf("Digite o número de pontos turisticos da primeira carta: \n");
+    scanf("%d", &pontos_turisticos1);
+
+    printf("Digite a area da primeira carta: \n");
+    scanf("%f", &area1);
+
+    printf("Digite o PIB da primeira carta: \n");
+    scanf("%f", &pib1);
+
+//Dados para trunfo do País 2
+    char pais2[30], codigo2[5], capital2[50];
+    int populacao2, pontos_turisticos2;
+    float area2, pib2;
+
+//Área para dados do País 2
+
+    printf("Digite o País da segunda carta: \n");
+    scanf("%s", pais2); 
+
+    printf("Digite o codigo da segunda carta: \n");
+    scanf("%s", codigo2);
+
+    printf("Digite a Capital da segunda carta: \n");
+    scanf("%s", capital2);
+
+    printf("Digite a populacao da segunda carta: \n");
+    scanf("%d", &populacao2);
+    
+    printf("Digite o número de pontos turisticos da segunda carta: \n");
+    scanf("%d", &pontos_turisticos2);
+
+    printf("Digite a area da segunda carta: \n");
+    scanf("%f", &area2);
+
+    printf("Digite o PIB da segunda carta: \n");
+    scanf("%f", &pib2);
+
+  //Área para mostrar os dados das cartas 1 e 2
+
+    printf("\n--- Dados da Primeira Carta ---\n");
+
+    printf("O País é: %s \n", pais1);
+    printf("O Código é: %s \n", codigo1);
+    printf("A capital é: %s \n", capital1);
+    printf("A Populacao é de: %d habitantes \n", populacao1);
+    printf("Número de Pontos Turisticos: %d \n", pontos_turisticos1);
+    printf("Area Total: %.2f  km² \n", area1);
+    printf("O PIB é de: US$ %.2f \n", pib1);
+
+    
+    printf("\n--- Dados da Segunda Carta ---\n");
+
+    printf("O País é: %s \n", pais2);
+    printf("O Código é: %s \n", codigo2);  
+    printf("A Capital é: %s \n", capital2);
+    printf("A Populacao é de: %d habitantes \n", populacao2);
+    printf("Número de Pontos Turisticos: %d \n", pontos_turisticos2);
+    printf("Area Total: %.2f  km² \n", area2);
+    printf("O PIB é de: US$ %.2f \n", pib2);
+
+
 
 return 0;
 } 
